4bb50f420a4d4629c4e9d9fdf8ba188887423124
Walliski <walliski@gmail.com>
2015-01-23 14:50:32 +0200

    [Rotatelogos] Removed unused stuff

--- apps/rotatelogos/urls.py
+++ apps/rotatelogos/urls.py
@@ -4,7 +4,6 @@ from django.views.generic.base import RedirectView
 
 
 urlpatterns = patterns('',
-	#url(r'^$', views.index, name='rotatelogos'),
 	url(r'^(?P<index>\d+)/?$', views.get_logo, name='get_logo'),
 	url(r'^$', RedirectView.as_view(url='/rotatelogos/0/', permanent=False)),
 )
