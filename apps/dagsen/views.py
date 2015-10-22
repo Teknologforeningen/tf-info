be2c41d17e9ee402435b2f3f3c2e12cc72591d4b
Oskar Ehnstrom <oskar@ehnstrom.net>
2015-10-22 11:56:59 +0300

    Use direct link to ipcam in template

--- apps/dagsen/views.py
+++ apps/dagsen/views.py
@@ -40,23 +40,5 @@ def index(request, language='sv'):
       return HttpResponse("Error parsing json from api", status=500)
 
   menu['cachebuster'] = random.randint(0, 99999)
+  menu['cam_url'] = cam_url
   return render_to_response('dagsen/index.html', menu, context_instance=RequestContext(request))
-
-@cache_page(5)
-def queuecam(request):
-  try:
-    stream=urllib.urlopen(cam_url)
-    bytes=''
-    for x in range(0, 1000):
-      bytes+=stream.read(1024)
-      a = bytes.find('\xff\xd8')
-      b = bytes.find('\xff\xd9')
-      if a!=-1 and b!=-1:
-          jpg = bytes[a:b+2]
-          bytes= bytes[b+2:]
-          response = HttpResponse(jpg, content_type="image/jpeg")
-          response['Cache-Control'] = 'max-age=0, no-cache, no-store'
-          return response
-  except:
-    return HttpResponse("Unable to access camera.", status=500)
-
