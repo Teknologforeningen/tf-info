3464f8b7571596336ab05d42305061aa237d9d97
Morakh <jussi.paakkola@aalto.fi>
2017-04-11 12:19:35 +0300

    Add primary implementation of countdown app

---
+++ apps/countdown/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import patterns, url
+from countdown import views
+
+urlpatterns = patterns('',
+    url(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2})/(?P<hour>[0-9]{2})/(?P<text>\w+)/$', views.index, name='countdown'),
+)
---
+++ apps/countdown/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.http import HttpResponse
+from django.utils.http import urlencode
+
+import json
+import urllib2
+
+
+
+def index(request, year, month, day, hour, text):
+
+	params = {
+		'year': year,
+		'month': int(month) - 1, #JS takes months in retarded format...
+		'day': day,
+		'text': text,
+		'hour': int(hour) - 1 #JS also takes hours in retarded format...
+	}
+
+	return render_to_response('countdown/index.html', params, context_instance=RequestContext(request))
