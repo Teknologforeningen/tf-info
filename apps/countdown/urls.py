9a608cc3f4c19b291b4218dfaa820ea548080a8e
Thomas Langenskiöld <tlangens@gmail.com>
2017-04-11 15:54:40 +0300

    allow spaces for title in url, style changes

--- apps/countdown/urls.py
+++ apps/countdown/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, url
 from countdown import views
 
 urlpatterns = patterns('',
-    url(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2})/(?P<hour>[0-9]{2})/(?P<text>\w+)/$', views.index, name='countdown'),
+    url(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2})/(?P<hour>[0-9]{2})/(?P<text>.+)/$', views.index, name='countdown'),
 )
