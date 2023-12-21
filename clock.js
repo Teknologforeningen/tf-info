cd24a71e4da305c9c416d38a0d2237181ea6ca54
Markus Nyman <markus@nyman.dev>
2023-12-22 00:20:13 +0200

    add clock

---
+++ clock.js
@@ -0,0 +1,18 @@
+const timeElement = document.getElementById("time")
+const dateElement = document.getElementById("date")
+
+/**
+* @param {Date} date 
+*/
+function updateClock(now) {
+    const time = now.toLocaleTimeString('sv')
+    const date = now.toLocaleDateString('de')
+
+    timeElement.innerText = time
+    dateElement.innerText = date
+}
+
+updateClock(new Date())
+setInterval(() => {
+    updateClock(new Date())
+}, 1000);
