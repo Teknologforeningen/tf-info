f8acbd465ff8a9bd93e9e49bdf8e36fd5df530f4
Markus Nyman <markus@nyman.dev>
2024-04-23 15:20:52 +0300

    show next day's menu already at 15

--- dagsen.ts
+++ dagsen.ts
@@ -24,7 +24,7 @@ export async function fetchMenu(
 
 function dayNumber(now: Date): 0 | 1 {
   const weekday = now.getDay();
-  return 0 < weekday && weekday < 6 && now.getHours() > 15 ? 1 : 0;
+  return 0 < weekday && weekday < 6 && now.getHours() >= 15 ? 1 : 0;
 }
 
 export async function fetchAlaCarte(): Promise<string | null> {
